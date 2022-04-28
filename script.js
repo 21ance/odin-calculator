cd7448cfd1e5a03c7fc2da0febdb6f1a4cd7e387
LanceL <lancelopez46@gmail.com>
2022-04-28 17:47:32 +0800

    start from scratch..

--- script.js
+++ script.js
@@ -1,85 +1,11 @@
-const solution = document.querySelector('#solution')
-const result = document.querySelector('#result')
-const btnNum = document.querySelectorAll('button')
-const btnOp = document.querySelectorAll('.number-operators .right button')
-const clear = document.querySelector('#clear')
-const btnEquals = document.querySelector('#equals')
+const topScreen = document.querySelector('[data-top-screen]')
+const botScreen = document.querySelector('[data-bottom-screen]')
 
-let screenNumber = ''
-let number1 = 0
-let number2 = 0
-let number3 = 0
-let op = ''
+const btnNumbers = document.querySelectorAll('[data-number]')
+const btnOperators = document.querySelectorAll('[data-operator]')
 
-function populateScreen() {
-    btnNum.forEach((number) => {
-        if (number.id >= 0) {
-            number.addEventListener('click', () => {
-
-                screenNumber += `${number.id}`
-
-                result.textContent = `${screenNumber}`
-            })
-        }
+btnNumbers.forEach(number => {
+    number.addEventListener('click', (e) => {
+        console.log(e.target)
     })
-}
-
-function saveNumbers() {
-    btnOp.forEach((operator) => {
-        operator.addEventListener('click', () => {
-
-            switch (operator.id) {
-                case '+':
-                    number1 = +screenNumber
-                    op = operator.id
-                    console.log(number1)
-                    solution.textContent = `${number1} ${operator.id}`
-                    result.textContent = number2
-                    screenNumber = ''
-            }
-        })
-    })
-}
-saveNumbers()
-
-function equals() {
-    btnEquals.addEventListener('click', () => {
-        number2 = +screenNumber
-        number3 = operate(op, number1, number2)
-        solution.textContent = `${number1} ${op} ${number2}`
-        result.textContent = `${number3}`
-    })
-}
-equals()
-
-function add(num1, num2) {
-    return num1 + num2;
-}
-
-function subtract(num1, num2) {
-    return num1 - num2;
-}
-
-function multiply(num1, num2) {
-    return num1 * num2;
-}
-
-function divide(num1, num2) {
-    return num1 / num2;
-}
-
-function operate(operator, num1, num2) {
-    switch (operator) {
-        case '+':
-            return add(num1, num2)
-        case '-':
-            return subtract(num1, num2)
-        case '*':
-            return multiply(num1, num2)
-        case '/':
-            return divide(num1, num2)
-    }
-}
-
-populateScreen()
-// console.log(operate('-', 10, 10))
+})
